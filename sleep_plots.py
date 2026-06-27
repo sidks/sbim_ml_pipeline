1f8c190499824597086aaabcd70c8a665b0416f1
Siddharth Kalyanasundaram <sidks75@gmail.com>
2026-06-27 18:55:45 +0530

    Update sleep_plots.py

--- sleep_plots.py
+++ sleep_plots.py
@@ -7,9 +7,9 @@ from matplotlib.dates import DateFormatter
 
 # File paths
 BASE_DIR = "/srv/repos/raddlab_datascience/cingo-db-data-extractor/output"
-ACCEL_CSV = os.path.join(BASE_DIR, "iwellnt_device_usage_2026-03-27_to_2026-06-22.csv")
-SURVEY_CSV = os.path.join(BASE_DIR, "iwellnt_survey_data_2026-03-27_to_2026-06-22.csv")
-output_file = "device_usage_sample_coverage_iwellnt.png"
+ACCEL_CSV = os.path.join(BASE_DIR, "test7_device_usage_2026-03-27_to_2026-06-22.csv")
+SURVEY_CSV = os.path.join(BASE_DIR, "test7_survey_data_2026-03-27_to_2026-06-22.csv")
+output_file = "device_usage_sample_coverage_test7.png"
 TIMEZONE = "America/Denver"
 
 # ------------------------------------------------------------------
