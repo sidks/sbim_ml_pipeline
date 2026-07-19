2196dc0d13c9feccfd3e0f0b39d0c5bcf53b2ec0
Siddharth Kalyanasundaram <sidks75@gmail.com>
2026-07-19 16:44:02 -0600

    Update sleep_features_analysis.py

--- sleep_features_analysis.py
+++ sleep_features_analysis.py
@@ -12,12 +12,12 @@ import pytz
 ###############################################################################
 BASE_DIR = Path("/srv/repos/raddlab_datascience/cingo-db-data-extractor/output")
 
-ACCEL_CSV = BASE_DIR / "iwellnt_acceleration_2026-03-27_to_2026-06-27.csv"
-LIGHT_CSV = BASE_DIR / "iwellnt_light_2026-03-27_to_2026-06-27.csv"
-DEVICE_CSV = BASE_DIR / "iwellnt_device_usage_2026-03-27_to_2026-06-27.csv"
-SURVEY_CSV = BASE_DIR / "iwellnt_survey_data_2026-03-27_to_2026-06-27.csv"
+ACCEL_CSV = BASE_DIR / "iwellrk_acceleration_2026-03-27_to_2026-06-27.csv"
+LIGHT_CSV = BASE_DIR / "iwellrk_light_2026-03-27_to_2026-06-27.csv"
+DEVICE_CSV = BASE_DIR / "iwellrk_device_usage_2026-03-27_to_2026-06-27.csv"
+SURVEY_CSV = BASE_DIR / "iwellrk_survey_data_2026-03-27_to_2026-06-27.csv"
 
-OUTPUT_CSV = "/srv/repos/sbim_ml_pipeline/sleep_analysis_iwellnt_new_logic.csv"
+OUTPUT_CSV = "/srv/repos/sbim_ml_pipeline/sleep_analysis_iwellrk_new_logic.csv"
 
 ###############################################################################
 # SURVEY GROUND TRUTH PARSING WITH DAYTIME ERROR CORRECTION
